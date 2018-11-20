99975e8104967120f7802014587206c078494fd1
runclimbhike <runclimbhike@users.noreply.github.com>
2018-11-19 21:02:26 -0600

    create, append and write a list to text file

---
+++ Open.py
@@ -0,0 +1,13 @@
+# create text file and write to it
+#with open("./example.txt", "w") as File1:
+#    File1.write("This is line 1.")
+
+# Now let's append to the file and \n to add it to a new line.
+#with open("./example.txt", "a") as File1:
+#    File1.write("This is line 4.\n")
+
+# Now let's write a list to a file
+#Lines = ["This is line 1.\n", "This is line 2.\n", "This is line 3.\n"]
+#with open("./example2.txt", "w") as File1:
+#    for line in Lines:
+#        File1.write(line)
